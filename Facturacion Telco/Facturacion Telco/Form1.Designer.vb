b5d251393b67eea9b0762dbe2cba0a9c59e4bf06
Christian Andres Arteaga <caarteagag@gmail.com>
2015-04-29 15:05:44 -0500

    Comit Inicial
    Carga de archivos base de la aplicacion

---
+++ Facturacion Telco/Facturacion Telco/Form1.Designer.vb
@@ -0,0 +1,291 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Principal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Clientlist = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ClientNit = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ClientDireccion = New System.Windows.Forms.TextBox()
+        Me.ClientTel = New System.Windows.Forms.TextBox()
+        Me.ClientCondi = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ClientOc = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Nextbt = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.ClientOc)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.ClientCondi)
+        Me.GroupBox1.Controls.Add(Me.ClientTel)
+        Me.GroupBox1.Controls.Add(Me.ClientDireccion)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.ClientNit)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Clientlist)
+        Me.GroupBox1.Location = New System.Drawing.Point(123, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(385, 219)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Encabezado"
+        '
+        'Clientlist
+        '
+        Me.Clientlist.FormattingEnabled = True
+        Me.Clientlist.Location = New System.Drawing.Point(134, 24)
+        Me.Clientlist.Name = "Clientlist"
+        Me.Clientlist.Size = New System.Drawing.Size(234, 21)
+        Me.Clientlist.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Cliente:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Dirección:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Telefono:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 132)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(110, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Condiciones de pago:"
+        '
+        'ClientNit
+        '
+        Me.ClientNit.Location = New System.Drawing.Point(134, 51)
+        Me.ClientNit.Name = "ClientNit"
+        Me.ClientNit.Size = New System.Drawing.Size(234, 20)
+        Me.ClientNit.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 54)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Nit:"
+        '
+        'ClientDireccion
+        '
+        Me.ClientDireccion.Location = New System.Drawing.Point(134, 77)
+        Me.ClientDireccion.Name = "ClientDireccion"
+        Me.ClientDireccion.Size = New System.Drawing.Size(234, 20)
+        Me.ClientDireccion.TabIndex = 7
+        '
+        'ClientTel
+        '
+        Me.ClientTel.Location = New System.Drawing.Point(134, 103)
+        Me.ClientTel.Name = "ClientTel"
+        Me.ClientTel.Size = New System.Drawing.Size(150, 20)
+        Me.ClientTel.TabIndex = 8
+        '
+        'ClientCondi
+        '
+        Me.ClientCondi.FormattingEnabled = True
+        Me.ClientCondi.Location = New System.Drawing.Point(134, 129)
+        Me.ClientCondi.Name = "ClientCondi"
+        Me.ClientCondi.Size = New System.Drawing.Size(150, 21)
+        Me.ClientCondi.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 159)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Orden de compra:"
+        '
+        'ClientOc
+        '
+        Me.ClientOc.Location = New System.Drawing.Point(134, 156)
+        Me.ClientOc.Name = "ClientOc"
+        Me.ClientOc.Size = New System.Drawing.Size(150, 20)
+        Me.ClientOc.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 188)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Fecha:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Enabled = False
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(245, 182)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(123, 20)
+        Me.DateTimePicker1.TabIndex = 14
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(134, 185)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(110, 17)
+        Me.CheckBox1.TabIndex = 13
+        Me.CheckBox1.Text = "Usar fecha actual"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Nextbt
+        '
+        Me.Nextbt.Location = New System.Drawing.Point(368, 237)
+        Me.Nextbt.Name = "Nextbt"
+        Me.Nextbt.Size = New System.Drawing.Size(140, 37)
+        Me.Nextbt.TabIndex = 1
+        Me.Nextbt.Text = "Siguiente"
+        Me.Nextbt.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 40)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Clientes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 58)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 40)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Facturas"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 191)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(105, 40)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Config. General"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.ForeColor = System.Drawing.Color.Silver
+        Me.Label8.Location = New System.Drawing.Point(120, 237)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(121, 39)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Software de facturacion" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Telco Electronica LTDA" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "2014 - 2016 (c)"
+        '
+        'Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(520, 286)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Nextbt)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Principal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Facturación"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Clientlist As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents ClientOc As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents ClientCondi As System.Windows.Forms.ComboBox
+    Friend WithEvents ClientTel As System.Windows.Forms.TextBox
+    Friend WithEvents ClientDireccion As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ClientNit As System.Windows.Forms.TextBox
+    Friend WithEvents Nextbt As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
